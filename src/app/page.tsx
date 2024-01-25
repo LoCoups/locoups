be333f1cd67d8f5f4631c1ddc64d93727ef4f070
Daniel Kim <danieldkim.42@gmail.com>
2024-01-25 14:50:45 -0800

    added coupon component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,49 @@
 import Image from 'next/image'
+import Coupon from './components/coupon'
+
+const coupons = [
+  {
+    name: "McDonalds",
+    deal: "$5 off $20"
+  },
+  {
+    name: "Taco Bell",
+    deal: "$4 off $20"
+  },
+  {
+    name: "Chick-Fil-A",
+    deal: "$3 off $20"
+  },
+  {
+    name: "Raising Canes",
+    deal: "$6 off $20"
+  },
+  {
+    name: "Raising Canes",
+    deal: "$6 off $20"
+  },
+  {
+    name: "Raising Canes",
+    deal: "$6 off $20"
+  },
+  {
+    name: "Raising Canes",
+    deal: "$6 off $20"
+  },
+]
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Home</div>
+    <main className="flex w-full flex-wrap min-h-screen overflow-y-auto p-12">
+      {coupons.map((coupon, index) => {
+        return (
+          <Coupon
+            key={index}
+            name={coupon.name}
+            deal={coupon.deal}
+          />
+        )
+      })}
     </main>
   )
 }
