e4d70043f327039f287000018496101a2a1315ad
Nathan Agbayani <nathan.pagbayani@gmail.com>
2024-01-25 16:18:12 -0800

    Fixed Nav Bar Height Changing
    Fixed issue where Nav Bar height changes when Dropdown suggestions  extend from the search bar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -3,11 +3,14 @@ import Searchbar from "./searchbar";
 
 export default function Navbar() {
   return (
-    <nav className='bg-gray-800 p-4 flex justify-between '>
+    <nav
+      className='bg-gray-800 p-4 flex justify-between'
+      style={{ height: "5em" }}
+    >
       <div>
         <h2>LoCoups</h2>
       </div>
-      <Searchbar/> 
+      <Searchbar />
       <div className='flex '>
         <h4>login</h4>
         <h4>signup</h4>
