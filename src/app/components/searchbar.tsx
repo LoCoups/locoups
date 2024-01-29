216051c650065bd169591c0437c5e4ee2fb968d6
Nathan Agbayani <nathan.pagbayani@gmail.com>
2024-01-29 14:08:59 -0800

    Update Search Bar

--- src/app/components/searchbar.tsx
+++ src/app/components/searchbar.tsx
@@ -64,10 +64,10 @@ export default function Searchbar() {
           />
           <button
             type='submit'
-            className='absolute block top-0 end-0 p-2.5 z-20 h-14 text-sm font-medium text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+            className='absolute top-0 end-0 p-2.5 z-20 h-14 w-18 flex items-center justify-center text-sm font-medium text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
           >
             <svg
-              className='w-3 h-4'
+              className='w-8 h-8'
               aria-hidden='true'
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
